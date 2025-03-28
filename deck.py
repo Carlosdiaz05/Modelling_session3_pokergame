d564fd5af0ed1f758c8e379eda11de39686e9cb8
Carlos <carlosdj0305@gmail.com>
2025-03-28 10:50:28 +0100

    Poker game, fixed

--- deck.py
+++ deck.py
@@ -2,7 +2,7 @@ import random
 class Card:
     RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     SUITS = ["♣", "♦", "♥", "♠"]
-    # SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    # SUITS = ["clubs", "diamonds", "hearts", "spades"]
     def __init__(self, rank, suit):
         if rank not in self.RANKS:
             raise ValueError("invalid rank")
@@ -17,13 +17,13 @@ class Card:
 
     @property
     def suit(self):
-        return f"{self._rank}{self._suit}"
+        return self._suit
 
     def __str__(self):
         return f"{self._rank}{self._suit}"
 
     def __repr__(self):
-        return self.__str__() #repr is the same as str
+        return self.__str__() # repr is the same as str
 
 class Deck:
     def __init__(self):
@@ -46,12 +46,12 @@ class Deck:
     def deal(self):
         return self.cards.pop(0)
 
-c1 = Card("A", "♣")
-print(c1.suit, c1.rank)
-deck = Deck()
-print(deck)
-deck.shuffle()
-print(deck)
-print(deck.deal())
-print(deck)
-
+if __name__ == "__main__":
+    c1 = Card("A", "♣")
+    print(c1.suit, c1.rank)
+    deck = Deck()
+    print(deck)
+    deck.shuffle()
+    print(deck)
+    print(deck.deal())
+    print(deck)
--- game.py
+++ game.py
@@ -1,39 +1,37 @@
 from deck import Deck, Card
 
+
 class Hand:
-    def __init__(self, deck):
-        cards = []
+    def __init__(self):
+        hand = []
         for i in range(5):
-            cards.append(deck.deal())
-        self._cards = cards
+            hand.append(deck.deal())
+        self._hand = hand
 
     @property
-    def cards(self):
-        return self._cards
+    def hand(self):
+        return self._hand
 
     def __str__(self):
-        return str(self.cards)
+        return str(self.hand)
 
     @property
     def is_flush(self):
-        for card in self.cards[1:]:
-            if self.cards[0].suit != card.suit:
+        for card in self.hand:
+            if card.suit != self.hand[0].suit:
                 return False
         return True
 
 
+# deck = Deck()
+# deck.shuffle()
+# h = Hand()
+# print(h)
+
 while True:
     deck = Deck()
     deck.shuffle()
-    hand = Hand(deck)
-    if hand.is_flush:
-        print(hand)
+    h = Hand()
+    if h.is_flush:
+        print(h)
         break
-
-
-deck = Deck()
-deck.shuffle()
-hand = Hand(deck)
-print(hand)
-#done
-
